89c87f77a158990a9c1cae3b28358059953e87a0
Adam Lyu <flyu008@uottawa.ca>
2024-10-03 09:20:49 -0400

    show student info with first name, last name, and enrollment date in admin panel

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'enrollment_date')
